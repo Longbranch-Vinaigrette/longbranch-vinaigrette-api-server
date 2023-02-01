daef67c77dcb1f1c1ed894a469878ada7541a7ef
Felix <fedecraft3312@gmail.com>
2023-01-31 23:36:25 -0300

    Added route isAppRunning

---
+++ DevToolsServer/src/routes/app/status/isAppRunning.py
@@ -0,0 +1,68 @@
+import json
+
+from django.http import HttpRequest
+
+from src.submodules.dev_tools_utils.django_utils import DjangoUtils
+from src.submodules.dev_tools_utils.Debug import Debug
+from src.submodules.dev_tools_utils.app_manager import AppManager
+
+
+class Main:
+    def __init__(self, route: str):
+        self.route = route
+        self.debug = False
+
+    def post(self, request: HttpRequest):
+        """Check if the app is running"""
+        dj_utils = DjangoUtils(request)
+        data = dj_utils.validate_json_content_type(request)
+
+        # If there is "debug" in data it means that there was an error
+        if not "debug" in data:
+            body: dict = json.loads(request.body.decode("utf-8"))
+
+            # Check if the required data was given
+            try:
+                # Inform the user in case the data is in a bad format.
+                # Set error message to give to the user
+                msg = "Key error, path was not given."
+
+                # Get the path
+                path = body["path"]
+                if not path or not isinstance(path, str):
+                    msg = "No path given, path is not a string or bad format."
+                    raise Exception(msg)
+            except Exception as ex:
+                print("Exception: ", ex)
+                data = {
+                    "debug": Debug(msg, error=True,
+                                   state="danger").get_message(),
+                }
+                return dj_utils.get_json_response(data)
+
+            try:
+                return dj_utils.get_json_response({
+                    "data": {
+                        "isAppRunning": AppManager(path).is_app_running(),
+                    }
+                })
+            except Exception as ex:
+                print("Exception: ", ex)
+                data = {
+                    "debug": Debug("Unknown error.", error=True,
+                                   state="error").get_message(),
+                }
+
+        return dj_utils.get_json_response(data)
+
+    def get(self, request: HttpRequest):
+        raise Exception("This route doesn't handle the given method.")
+
+    def handle_request(self, req: HttpRequest):
+        """Handle request"""
+        if req.method == "POST":
+            return self.post(req)
+        elif req.method == "GET":
+            return self.get(req)
+        else:
+            raise Exception("This route doesn't handle the given method.")
