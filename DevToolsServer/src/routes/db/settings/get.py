b84406e4e510b324032c1f9aefdd7778790c884f
Felix <fedecraft3312@gmail.com>
2023-02-06 23:55:19 -0300

    Getting data from the settings table now works

---
+++ DevToolsServer/src/routes/db/settings/get.py
@@ -0,0 +1,27 @@
+import json
+
+from src.submodules.dev_tools_utils.django_utils.route_object.Main import Main as RouteHandler
+import src.submodules.dev_tools_utils.django_utils as dj_utils
+from src.submodules.dev_tools_utils.dbs.SettingsTable import SettingsTable
+from src.submodules.dev_tools_utils.Debug import Debug
+
+
+class Main(RouteHandler):
+    def __init__(self, route: str):
+        super().__init__(route, post_fn=self.post_req)
+
+        # Note the data dependencies
+        self.set_data_dependencies(["key"])
+
+    def post_req(self, request):
+        """Post request"""
+        body: dict = json.loads(request.body.decode("utf-8"))
+        key = body["key"]
+        print("Get key: ", key)
+
+        # Upsert the data
+        table = SettingsTable()
+        data = table.get(key)
+        print("Data: ", data)
+
+        return dj_utils.get_json_response({"value": data, })
