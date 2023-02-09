265e72b973890051185cbc4ffc940f0ad03f8bad
Felix <fedecraft3312@gmail.com>
2023-02-09 20:46:56 -0300

    Reverted 2 commits and fixed errors with submodules

--- DevToolsServer/urls.py
+++ DevToolsServer/urls.py
@@ -1,18 +1,22 @@
+"""DevToolsServer URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-Create app
-python3.10 manage.py startapp APP_NAME
-"""
-import os
-import pprint
+from django.contrib import admin
+from django.urls import include, path
 
-from src.submodules.dev_tools_utils.dynamic_imports.django_routes import DjangoRoutes
-
-
-routes_path = f"{os.getcwd()}{os.path.sep}src{os.path.sep}routes"
-djroutes = DjangoRoutes(routes_path, handle_request=False)
-urlpatterns = djroutes.get_routes_as_urlpatterns()
-
-debug = False
-if debug:
-    print("Urlpatterns:")
-    pprint.pprint(urlpatterns)
+urlpatterns = [
+    path("", include("urls")),
+    path('admin/', admin.site.urls),
+]
